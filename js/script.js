480b8fab181321c9ea34fff0917ca4873c0ae89e
JCarloGarcia <sk8pro9015@gmail.com>
2024-06-12 18:45:40 +0200

    Ejercicio-finalizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+
+ const lista = document.getElementById('lista');
+ const  agregar = document.getElementById('agregar');
+
+ agregar.addEventListener('click', function (){
+    let elementoNuevo = prompt('Ingrese aquí su elemento');
+    if (elementoNuevo){
+        let li = document.createElement('li');
+        li.textContent = elementoNuevo;
+        lista.appendChild(li);
+    };
+   
+ });
+
